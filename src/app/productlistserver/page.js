941f0efaedeaca30a68e6d2b39211d1b52219543
Talha Waleed <talhawaleed460@gmail.com>
2025-01-22 04:19:08 -0800

    ADD Fetching on client side and on server side

---
+++ src/app/productlistserver/page.js
@@ -0,0 +1,60 @@
+import Link from "next/link"
+async function productList(){
+    const  response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+    return data.products;
+}
+export default async function ProductListServer ()
+
+{
+    let products =await productList();
+    console.log(products)
+    return (
+        <>
+        <h3 className="text-center font-bold  m-6 text-3xl">App With Server Side Rendring </h3>
+        <h2 className="font-bold text-2xl  ml-4">Product List </h2>
+
+   <div className="flex flex-col">
+                <div className="-m-1.5 overflow-x-auto">
+                    <div className="p-1.5 min-w-full inline-block align-middle">
+                        <div className="overflow-hidden">
+                            <table className="min-w-full divide-y divide-gray-200">
+
+                                <thead>
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Name</th>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Price</th>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Rating</th>
+                                        <th scope="col" className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Stok</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody className="divide-y divide-gray-200 ">
+
+                                    {products.map(item => (
+                                        <tr key={item.id}>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{item.title}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{item.price}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{item.rating}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-end text-sm text-gray-800">{item.stock}</td>
+                                        </tr>
+                                    ))}
+
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+ <Link
+                href="/"
+                className="text-orange-700 bg-slate-400 rounded-lg p-5 m-3 flex justify-center  hover:bg-blue-700 hover:text-white">
+                Go To home
+            </Link>
+        </>
+    )
+
+
+}
---
+++ src/app/products/page.js
@@ -0,0 +1,73 @@
+'use client'
+import Link from "next/link"
+import './product.css'
+import { useEffect, useState } from 'react'
+export default function Products() {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch("https://dummyjson.com/products");
+                const data = await response.json();
+                setProducts(data.products || []);  // Correcting to set 'data.products' directly
+            } catch (error) {
+                console.error('Failed to fetch products:', error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    console.log(products, '============')
+
+    return (
+        <>
+            <h3 className=" m-10">Product List </h3>
+
+            <div className="flex flex-col">
+                <div className="-m-1.5 overflow-x-auto">
+                    <div className="p-1.5 min-w-full inline-block align-middle">
+                        <div className="overflow-hidden">
+                            <table className="min-w-full divide-y divide-gray-200">
+
+                                <thead>
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Name</th>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Price</th>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Rating</th>
+                                        <th scope="col" className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Stok</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody className="divide-y divide-gray-200 ">
+
+                                    {products.map(item => (
+                                        <tr key={item.id}>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{item.title}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{item.price}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{item.rating}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-end text-sm text-gray-800">{item.stock}</td>
+                                        </tr>
+                                    ))}
+
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <Link
+                href="/"
+                className="text-red-700 bg-slate-200 rounded-lg p-5 m-3 flex justify-center  hover:bg-blue-700 hover:text-white">
+                Go To home
+            </Link>
+        </>
+
+    )
+
+}
